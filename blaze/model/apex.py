6adb45ae6b96b6cf4d6e58e0f7dae97ed963e036
Nikhil Kansal <nkansal96@gmail.com>
2019-04-07 20:19:51 -0700

    add apex model

---
+++ blaze/model/apex.py
@@ -0,0 +1,36 @@
+""" This module defines the model for training and instantiating APEX agents """
+import ray
+import ray.tune as tune
+
+from blaze.config.environment import EnvironmentConfig
+from blaze.config.train import TrainConfig
+from blaze.environment import Environment
+
+def train(train_config: TrainConfig, env_config: EnvironmentConfig):
+  """ Trains an APEX agent with the given training and environment configuration """
+  ray.init(num_cpus=train_config.num_cpus)
+  name = train_config.experiment_name
+  total_urls = sum(len(group.resources) for group in env_config.push_groups)
+  tune.run_experiments({
+    name : {
+      "run": "APEX",
+      "env": Environment,
+      "stop": {
+        "timesteps_total": train_config.max_timesteps,
+      },
+      "checkpoint_at_end": True,
+      "checkpoint_freq": 1,
+      "max_failures": 20,
+      "config": {
+        "sample_batch_size": 50,
+        "train_batch_size": 512,
+        "timesteps_per_iteration": total_urls,
+        "target_network_update_freq": total_urls * 5,
+        "batch_mode": "complete_episodes",
+        "collect_metrics_timeout": 1200,
+        "num_workers": train_config.num_cpus//2,
+        "num_gpus": 0,
+        "env_config": env_config,
+      },
+    },
+  }, resume='prompt')
