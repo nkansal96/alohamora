6969d2d6e5367548a0dc3a8a82345b76f1249132
Nikhil Kansal <nkansal96@gmail.com>
2019-04-09 13:10:08 -0700

    move run function from tests to blaze.util

---
+++ blaze/util/cmd.py
@@ -0,0 +1,15 @@
+""" This module defines simple utilities to run subprocess commands """
+import subprocess
+from typing import List
+
+def run(cmd: List[str]) -> str:
+  """
+  run_cmd runs the specified command and returns what was printed to stdout. If the
+  process exits with return code != 0, it returns an empty string
+  """
+  try:
+    proc = subprocess.run(cmd, stdout=subprocess.PIPE)
+    proc.check_returncode()
+    return str(proc.stdout).strip()
+  except subprocess.CalledProcessError:
+    return ""
