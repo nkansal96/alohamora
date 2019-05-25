9162c6690e69dc0703d8f45f22fd406f295cbdbf
Nikhil Kansal <nkansal96@gmail.com>
2019-05-24 21:46:32 -0700

    remove incomplete analytics test

--- tests/command/test_analyze.py
+++
@@ -1,8 +0,0 @@
-from blaze.command.analyze import analyze
-from types import SimpleNamespace
-
-
-class TestAnalyze:
-    def test_analyze(self):
-        # analyze(["/tmp/train_results", "--site", "https://www.reddit.com"])
-        assert True
