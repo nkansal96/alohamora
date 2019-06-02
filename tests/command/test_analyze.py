809f6a21f242e905232a112b9f27bbbd2aaff284
Nikhil Kansal <nkansal96@gmail.com>
2019-06-02 14:56:04 -0700

    remove test_analyze (again)

--- tests/command/test_analyze.py
+++
@@ -1,8 +0,0 @@
-from blaze.command.analyze import analyze
-from types import SimpleNamespace
-
-
-class TestAnalyze:
-    def test_analyze(self):
-        analyze(["/tmp/eval_results_dir"])
-        assert True
