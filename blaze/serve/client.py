c7c859797e63b350143581c1aee9eff08c1ea163
Nikhil Kansal <nkansal96@gmail.com>
2019-06-03 21:53:00 -0700

    add client implementations and tests

---
+++ blaze/serve/client.py
@@ -0,0 +1,45 @@
+""" Defines a test client that queries the gRPC server to get a policy """
+from typing import List
+
+import grpc
+
+from blaze.config.client import NetworkType, DeviceSpeed
+from blaze.config.environment import Resource
+from blaze.proto import policy_service_pb2
+from blaze.proto import policy_service_pb2_grpc
+
+
+class Client:
+    """
+    A gRPC client that connects to the policy service and queries it for a policy
+    """
+
+    def __init__(self, channel: grpc.Channel):
+        self.channel = channel
+        self.stub = policy_service_pb2_grpc.PolicyServiceStub(channel)
+
+    def get_policy(
+        self,
+        url: str,
+        network_type: NetworkType,
+        device_speed: DeviceSpeed,
+        resources: List[Resource],
+        train_domain_globs: List[str],
+    ):
+        """ Queries the policy service for a push policy for the given configuration """
+        page = policy_service_pb2.Page(
+            url=url,
+            network_type=network_type.value,
+            device_speed=device_speed.value,
+            train_domain_globs=train_domain_globs,
+            resources=[
+                policy_service_pb2.Resource(url=res.url, size=res.size, type=res.type.value, timestamp=res.order)
+                for res in resources
+            ],
+        )
+
+        policy_res = self.stub.GetPolicy(page)
+        policy = {}
+        for item in policy_res.policy:
+            policy[item.source_url] = list(item.push_urls)
+        return policy
