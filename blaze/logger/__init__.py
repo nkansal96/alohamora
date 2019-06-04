f79586d8a97c7293588886ac1578a46578b3d1b4
Nikhil Kansal <nkansal96@gmail.com>
2019-06-03 21:42:28 -0700

    log to stderr instead of stdout

--- blaze/logger/__init__.py
+++ blaze/logger/__init__.py
@@ -1,5 +1,6 @@
 """ This module defines a package-wide standard logger """
 import os
+import sys
 
 import colorama
 
@@ -11,8 +12,12 @@ colorama.init()
 
 def get_default_logger():
     """ Creates the default logger """
+
+    def print_to_stderr(s: str):
+        sys.stderr.write(s + "\n")
+
     min_level = Level.DEBUG if "DEBUG" in os.environ else Level.INFO
-    return Logger(namespace="blaze", min_level=min_level)
+    return Logger(namespace="blaze", min_level=min_level, print_fn=print_to_stderr)
 
 
 logger = get_default_logger()  # pylint: disable=invalid-name  # disable for global access to logger
