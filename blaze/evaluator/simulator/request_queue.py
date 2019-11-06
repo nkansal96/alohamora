ffca37db9d7649f3a5a60110b078b5c396910254
Nikhil Kansal <nkansal96@gmail.com>
2019-11-05 16:51:29 -0800

    fix bug by copying node-queueitem map

--- blaze/evaluator/simulator/request_queue.py
+++ blaze/evaluator/simulator/request_queue.py
@@ -72,6 +72,7 @@ class RequestQueue:
         rq = RequestQueue(self.bandwidth_kbps, self.rtt_latency_ms)
         rq.queue = [copy.copy(qi) for qi in self.queue]
         rq.delayed = [copy.copy(qi) for qi in self.delayed]
+        rq.node_to_queue_item_map = {node: copy.copy(qi) for (node, qi) in self.node_to_queue_item_map.items()}
         rq.connected_origins = set(self.connected_origins)
         rq.tcp_state = copy.deepcopy(self.tcp_state)
         return rq
