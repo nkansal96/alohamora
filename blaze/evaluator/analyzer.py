1608ca220d95faf738e8a866b237d505cfad5588
Nikhil Kansal <nkansal96@gmail.com>
2019-11-07 21:07:09 -0800

    try -plt as reward function

--- blaze/evaluator/analyzer.py
+++ blaze/evaluator/analyzer.py
@@ -31,16 +31,12 @@ class Analyzer:
     def __init__(self, config: Config, client_environment: Optional[client.ClientEnvironment] = None):
         self.config = config
         self.client_environment = client_environment
-        self.min_plt = MIN_PAGE_LOAD_TIME
-        self.last_plt = None
         self.simulator = simulator.Simulator(config.env_config)
         self.log = logger.with_namespace("analyzer")
 
     def reset(self, client_environment: Optional[client.ClientEnvironment] = None):
         """ Resets the analyzer's state and optionally changes the client environment """
         self.client_environment = client_environment or self.client_environment
-        self.min_plt = MIN_PAGE_LOAD_TIME
-        self.last_plt = None
 
     def get_reward(self, policy: Policy):
         """
@@ -48,17 +44,6 @@ class Analyzer:
         the client environment and the page load. It calculates the reward based on the
         resulting speed index returned from Lighthouse
         """
-        self.log.debug("analyzing web page...")
         plt = self.simulator.simulate_load_time(self.client_environment, policy)
-        reward = 0
-        if plt < self.min_plt:
-            self.log.debug("received best page load time", plt=plt)
-            reward = BEST_REWARD_COEFF / plt
-        else:
-            a, b = sorted([plt, self.last_plt])
-            coeff = REGRESSION_REWARD_COEFF if plt > self.last_plt else PROGRESSION_REWARD_COEFF
-            self.log.debug("received {} page load time".format("better" if coeff > 0 else "worse"), plt=plt)
-            reward = coeff * (b / a)
-        self.min_plt = min(self.min_plt, plt)
-        self.last_plt = plt
-        return reward
+        self.log.debug("got page load time", plt=plt, reward=-plt)
+        return -plt
