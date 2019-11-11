9100bded5fd5f96ee2c706b9bdec7163a2c11e37
Nikhil Kansal <nkansal96@gmail.com>
2019-11-11 14:08:45 -0800

    add auto stopper

--- blaze/model/a3c.py
+++ blaze/model/a3c.py
@@ -1,10 +1,63 @@
 """ This module defines the model for training and instantiating A3C agents """
 
+from statistics import stdev
+
 from blaze.config.config import Config
 from blaze.config.train import TrainConfig
 from blaze.environment import Environment
+from blaze.logger import logger
 
 from .model import SavedModel
+
+
+WINDOW_SIZE = 50
+MAX_ITERATIONS = 400
+MIN_ITERATIONS = 100
+
+
+class StopCondition:
+    """
+    Implements a stateful stopping condition to automatically stop the training based on analyzing the running
+    episode reward mean over a certain window size. It also stop automatically if the number of training iterations
+    exceeds some maximum, but not before it exceeds some minimum.
+    """
+
+    def __init__(self):
+        self.log = logger.with_namespace("stop_condition")
+        self.num_iters = 0
+        self.episode_max_rewards = []
+        self.episode_min_rewards = []
+        self.episode_mean_rewards = []
+
+    def __call__(self, trial_id, result):
+        self.num_iters += 1
+
+        if "episode_reward_max" in result and "episode_reward_min" in result and "episode_reward_mean" in result:
+            self.log.debug("recording trial result", trial_id=trial_id, num_iters=self.num_iters)
+            self.episode_max_rewards.append(result["episode_reward_max"])
+            self.episode_min_rewards.append(result["episode_reward_min"])
+            self.episode_mean_rewards.append(result["episode_reward_mean"])
+        else:
+            self.log.warn("unable to record episode result", result=result, trial_id=trial_id)
+            return False
+
+        # truncate the rewards list to past `WINDOW_SIZE` iterations only
+        self.episode_max_rewards = self.episode_max_rewards[-WINDOW_SIZE:]
+        self.episode_min_rewards = self.episode_min_rewards[-WINDOW_SIZE:]
+        self.episode_mean_rewards = self.episode_mean_rewards[-WINDOW_SIZE:]
+
+        if self.num_iters > MIN_ITERATIONS:
+            self.log.debug(
+                "reward stats",
+                stdev_max=stdev(self.episode_max_rewards),
+                stdev_min=stdev(self.episode_min_rewards),
+                stdev_mean=stdev(self.episode_mean_rewards),
+            )
+            relative_stdev_based_stop = stdev(self.episode_mean_rewards) < 0.01 * self.episode_mean_rewards[-1]
+            if self.num_iters > MAX_ITERATIONS or relative_stdev_based_stop:
+                self.log.info("auto stopping", iters=self.num_iters)
+                return True
+        return False
 
 
 COMMON_CONFIG = {
@@ -31,7 +84,7 @@ def train(train_config: TrainConfig, config: Config):
             name: {
                 "run": "A3C",
                 "env": Environment,
-                "stop": {"timesteps_total": train_config.max_timesteps},
+                "stop": ray.tune.function(StopCondition()),
                 "checkpoint_at_end": True,
                 "checkpoint_freq": 10,
                 "max_failures": 1000,
