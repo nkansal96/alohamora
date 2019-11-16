a62a7d5993e68710c5a92397db792d27cb6bdaa1
Nikhil Kansal <nkansal96@gmail.com>
2019-11-15 16:34:20 -0800

    add simluator pct diff to analyzer

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -7,14 +7,18 @@ import pprint
 import sys
 
 
+def pct_diff(a, b):
+    return (a - b) / a
+
+
 def get_best_pct_diff(a, b):
-    return round(100 * min(filter(lambda d: d > -0.7, ((bi - ai) / ai for (ai, bi) in zip(a, b)))), 3)
+    return round(100 * min(filter(lambda d: d > -0.7, (pct_diff(bi, ai) for (ai, bi) in zip(a, b)))), 3)
 
 
 def get_best_val_pair(a, b):
     m, n = 0, 100
     for (ai, bi) in zip(a, b):
-        new_n = (bi - ai) / ai
+        new_n = pct_diff(bi, ai)
         if new_n < n and new_n > -0.7:
             m = (ai, bi)
             n = new_n
@@ -31,7 +35,7 @@ for result, path in zip(result_files, file_paths):
 
 results_map = {}
 for env, results in results_by_env.items():
-    pct_diff = sorted(
+    replay_pct_diff = sorted(
         [get_best_pct_diff(a["replay_server"]["without_policy"], a["replay_server"]["with_policy"]) for a in results]
     )
     replay_without_policy, replay_with_policy = list(
@@ -45,12 +49,22 @@ for env, results in results_by_env.items():
             ),
         )
     )
+    simulator_pct_diff = sorted(
+        [pct_diff(a["simluator"]["with_policy"], a["simulator"]["without_policy"]) for a in results]
+    )
     simulator_without_policy = sorted([a["simulator"]["without_policy"] for a in results])
     simulator_with_policy = sorted([a["simulator"]["with_policy"] for a in results])
     results_map[env] = {
-        "pct_diff": pct_diff,
-        "replay_server": {"without_policy": replay_without_policy, "with_policy": replay_with_policy},
-        "simulator": {"without_policy": simulator_without_policy, "with_policy": simulator_with_policy},
+        "replay_server": {
+            "pct_diff": replay_pct_diff,
+            "without_policy": replay_without_policy,
+            "with_policy": replay_with_policy,
+        },
+        "simulator": {
+            "pct_diff": simulator_pct_diff,
+            "without_policy": simulator_without_policy,
+            "with_policy": simulator_with_policy,
+        },
     }
 
 print(json.dumps(results_map, indent=4))
