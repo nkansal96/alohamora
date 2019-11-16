2d7dafe3d79fc1522a777f92355d4497af3d93ad
Nikhil Kansal <nkansal96@gmail.com>
2019-11-15 16:36:45 -0800

    fix typo

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -50,7 +50,7 @@ for env, results in results_by_env.items():
         )
     )
     simulator_pct_diff = sorted(
-        [pct_diff(a["simluator"]["with_policy"], a["simulator"]["without_policy"]) for a in results]
+        [pct_diff(a["simulator"]["with_policy"], a["simulator"]["without_policy"]) for a in results]
     )
     simulator_without_policy = sorted([a["simulator"]["without_policy"] for a in results])
     simulator_with_policy = sorted([a["simulator"]["with_policy"] for a in results])
