e334e444ccb83565ce0d149748a98dbc9bb50360
Murali Ramanujam <srmurali100@gmail.com>
2019-11-18 11:53:07 -0800

    Update NodeData.java
    - remove url and add setter and getter for size and type

--- tools/tree_diff/src/main/java/tree_diff/NodeData.java
+++ tools/tree_diff/src/main/java/tree_diff/NodeData.java
@@ -1,7 +1,23 @@
 package tree_diff;
 
 public class NodeData {
-    private String resourceURL;
     private double size;
     private String type;
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
 }
