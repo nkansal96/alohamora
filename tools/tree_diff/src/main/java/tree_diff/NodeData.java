715a7e0fd27b95a142817d511427612a515ef5e6
Murali Ramanujam <srmurali100@gmail.com>
2019-11-18 15:11:06 -0800

    Update NodeData.java
    - add a toString function, and add constructors

--- tools/tree_diff/src/main/java/tree_diff/NodeData.java
+++ tools/tree_diff/src/main/java/tree_diff/NodeData.java
@@ -4,6 +4,16 @@ public class NodeData {
     private double size;
     private String type;
 
+    public NodeData() {
+        this.size = -1;
+        this.type = "none";
+    }
+
+    public NodeData(double size, String type) {
+        this.size = size;
+        this.type = type;
+    }
+
     public double getSize() {
         return size;
     }
@@ -18,6 +28,11 @@ public class NodeData {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "type: " + this.type + ", size: " + this.size;
     }
 
 }
