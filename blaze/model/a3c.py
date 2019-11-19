e17f52a5650660e21f34d26c488fb9583bae3dc8
Nikhil Kansal <nkansal96@gmail.com>
2019-11-18 16:42:24 -0800

    adjust stdev based stop, add logging

--- blaze/model/a3c.py
+++ blaze/model/a3c.py
@@ -33,6 +33,7 @@ def stop_condition():
         num_iters += 1
 
         if "time_since_restore" in result and result["time_since_restore"] >= MAX_TIME_SECONDS:
+            log.info("auto stopping", time_seconds=result["time_since_restore"], iters=num_iters)
             return True
 
         if "episode_reward_max" in result and "episode_reward_min" in result and "episode_reward_mean" in result:
@@ -50,10 +51,10 @@ def stop_condition():
         if num_iters > MIN_ITERATIONS:
             stdev_min, stdev_mean, stdev_max = tuple(map(stdev, zip(*past_rewards)))
             log.debug("reward stats", stdev_min=stdev_min, stdev_mean=stdev_mean, stdev_max=stdev_max)
-            relative_stdev_based_stop = stdev_mean <= 0.05 * abs(past_rewards[-1][1])
+            relative_stdev_based_stop = stdev_mean <= 0.075 * abs(past_rewards[-1][1])
 
             if num_iters > MAX_ITERATIONS or relative_stdev_based_stop:
-                log.info("auto stopping", iters=num_iters)
+                log.info("auto stopping", time_seconds=result.get("time_since_restore", 0), iters=num_iters)
                 return True
 
         return False
