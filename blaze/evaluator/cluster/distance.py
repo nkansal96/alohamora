c3a483563f91195efe99200a3bd617a10e6d982b
Nikhil Kansal <nkansal96@gmail.com>
2019-11-20 23:08:08 -0800

    finish apted distance function

--- blaze/evaluator/cluster/distance.py
+++ blaze/evaluator/cluster/distance.py
@@ -1,8 +1,12 @@
 """ Implements distance functions for the """
+import json
 import math
-from typing import List
+from typing import Dict, List
+
+import requests
 
 from blaze.config.environment import EnvironmentConfig
+from blaze.evaluator.simulator import Simulator
 
 from .types import DistanceFunc
 
@@ -19,8 +23,26 @@ def euclidian_distance(a: List[float], b: List[float]) -> float:
 
 def create_apted_distance_function(port: int) -> DistanceFunc:
     """ Creates a distance function with a connection to the tree_diff server """
-    # TODO: implement this
+
+    def get_apted_tree(env_config: EnvironmentConfig) -> Dict:
+        sim = Simulator(env_config)
+        tree = {}
+        s = [sim.root]
+        while s:
+            curr = s.pop()
+            tree[curr.priority] = {
+                "size": curr.resource.size,
+                "type": str(curr.resource.type),
+                "children": [c.priority for c in curr.children],
+            }
+            s.extend(curr.children)
+        tree["length"] = len(tree)
+        return tree
+
     def apted_distance(a: EnvironmentConfig, b: EnvironmentConfig) -> float:
-        return 0.0
+        a_tree = json.dumps(get_apted_tree(a))
+        b_tree = json.dumps(get_apted_tree(b))
+        r = requests.get(f"http://localhost:{port}/getTreeDiff", params={"tree1": a_tree, "tree2": b_tree}).json()
+        return r["editDistance"]
 
     return apted_distance
