b1e2633ce1592c3a52773aa9cc67a7ccc4bffd02
Murali Ramanujam <murali.sr92@hotmail.com>
2019-11-21 14:43:34 -0800

    implementing Treediff (#35)
    * Create CostModel.java
    
    - implement the basic interface
    - functions simply return 0 cost for now
    
    * Create TreeNode.java
    
    - extend the Node class
    - define NodeDataStructure which will contain the attributes of a Node
    
    * Update CostModel.java
    
    - spacing issue
    - inherit all instead of specific class
    
    * Create InputParser.java
    
    - implement InputParser using TreeNode type
    
    * replace TreeNode with NodeData
    
    * Update InputParser.java
    
    - use NodeData instead of TreeNode
    
    * Update CostModel.java
    
    - parameterize CostModel to use NodeData as well
    
    * Update build.gradle
    
    - add json dependency
    
    * Update NodeData.java
    
    - remove url and add setter and getter for size and type
    
    * json parsing first version
    
    simple json parsing works now
    
    * Update AppTest.java
    
    - make test always pass for now
    
    * Update NodeData.java
    
    - add a toString function, and add constructors
    
    * Update InputParser.java
    
    - correctly parse input JSON to extract list of objects
    
    * implement webserver
    
    * Update InputParser.java
    
    - add comment about work in progress
    
    * Update TreeDiffWebService.java
    
    - implement cost differ
    - return response as json
    
    * Update TreeDiffWebService.java
    
    - remove debug print stmt
    
    * Update AppTest.java
    
    - add simple equality test
    
    * implement string parsing
    
    - infer parent-child relationships by doing a second pass over input json
    
    * Update TreeDiffWebService.java
    
    - extract out the diff computing functionality
    
    * Update InputParser.java
    
    - remove debug print
    
    * Update AppTest.java
    
    - add more tests to verify cost model consistency
    
    * Update CostModel.java
    
    - make deltas symmetric. insert/remove is worth more than renaming.
    
    * add id to node attributes
    
    - and add some debug statements to see the tree operations

---
+++ tools/tree_diff/src/main/java/tree_diff/InputParser.java
@@ -0,0 +1,52 @@
+package tree_diff;
+
+import eu.mihosoft.ext.apted.node.Node;
+import eu.mihosoft.ext.apted.parser.*;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.json.*;
+
+public class InputParser implements eu.mihosoft.ext.apted.parser.InputParser<NodeData> {
+    /**
+     * Converts the input tree passed as string (as a JSON)
+     * into the tree structure.
+     *
+     * @param s input tree as JSON.
+     * @return tree structure.
+     */
+    @Override
+    public Node<NodeData> fromString(String s) {
+        JSONObject inputNode = new JSONObject(s);
+        HashMap<Integer, Node<NodeData>> indexToNodeMapping= new HashMap<>();
+        HashMap<Integer, List<Integer>> parentToChildrenMapping = new HashMap<>();
+        NodeData newNodeData = null;
+        NodeData root = null;
+        if (inputNode.keySet().contains("length")) {
+            int length = inputNode.getInt("length");
+            for(int i = 0; i < length; i++) {
+                JSONObject currentObject = inputNode.getJSONObject(Integer.toString(i));
+                newNodeData = new NodeData(i, currentObject.getInt("size"), currentObject.getString("type"));
+                indexToNodeMapping.put(i, new Node<NodeData>(newNodeData));
+                if (!parentToChildrenMapping.keySet().contains(i)) {
+                    parentToChildrenMapping.put(i, new ArrayList<>());
+                }
+                for(Object child : currentObject.getJSONArray("children")) {
+                    parentToChildrenMapping.get(i).add((int)child);
+                }
+            }
+
+            for(int key : parentToChildrenMapping.keySet()) {
+                for(int child : parentToChildrenMapping.get(key)) {
+                    indexToNodeMapping.get(key).addChild(indexToNodeMapping.get(child));
+                }
+            }
+            return indexToNodeMapping.get(0);
+        } else {
+            return null;
+        }
+    }
+}
---
+++ tools/tree_diff/src/main/java/tree_diff/TreeDiffWebService.java
@@ -0,0 +1,70 @@
+package tree_diff;
+
+import com.sun.source.tree.Tree;
+import eu.mihosoft.ext.apted.distance.APTED;
+import eu.mihosoft.ext.apted.node.Node;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static spark.Spark.*;
+
+
+public class TreeDiffWebService {
+    public void parseAString() {
+        System.out.println(("server should be running now"));
+        InputParser parser = new InputParser();
+        Node<NodeData> t1 = parser.fromString("{\"0\":{\"children\":[1,2],\"size\":100,\"type\":\"text/html\"},\"1\":{\"children\":[],\"size\":75,\"type\":\"image/jpeg\"},\"2\":{\"children\":[],\"size\":50,\"type\":\"text/css\"},\"length\":3}");
+    }
+
+    public String getDiffBetweenStrings(String treeData1, String treeData2) {
+        InputParser parser = new InputParser();
+        Node<NodeData> t1 = parser.fromString(treeData1);
+        Node<NodeData> t2 = parser.fromString(treeData2);
+        APTED<CostModel, NodeData> apted = new APTED<>(new CostModel());
+        float result = apted.computeEditDistance(t1, t2);
+        JSONObject jsonOutput = new JSONObject();
+        jsonOutput.put("status", "success");
+        jsonOutput.put("message", "none");
+        jsonOutput.put("editDistance", result);
+        return jsonOutput.toString();
+    }
+
+    public static void runServer(int serverPort) {
+        TreeDiffWebService t = new TreeDiffWebService();
+        port(serverPort);
+        get("/getTreeDiff", (request, response) -> {
+            JSONObject jsonOutput = new JSONObject();
+            String treeData1 = request.queryMap().get("tree1").value();;
+            String treeData2 = request.queryMap().get("tree2").value();;
+            if (treeData1 == null || treeData2 == null) {
+                response.status(406);
+                jsonOutput.put("status", "error");
+                jsonOutput.put("editDistance", -1);
+                jsonOutput.put("message", "Error: query must include tree1 and tree2.");
+                return jsonOutput.toString();
+            } else {
+                try {
+                    return t.getDiffBetweenStrings(treeData1, treeData2);
+                } catch (JSONException e) {
+                    response.status(406);
+                    jsonOutput.put("status", "error");
+                    jsonOutput.put("editDistance", -1);
+                    jsonOutput.put("message", "Error: query was malformed. Please ensure it is a valid JSON.");
+                    return jsonOutput.toString();
+                } catch (Exception e) {
+                    response.status(500);
+                    jsonOutput.put("status", "error");
+                    jsonOutput.put("editDistance", -1);
+                    jsonOutput.put("message", "Error: Unable to parse input query.");
+                    return jsonOutput.toString();
+                }
+
+            }
+        });
+
+        post("/", (request, response) -> {
+            return "Post root";
+        });
+    }
+
+}
