cde69f46c6f1073b83da71c7a81403b21ecbf7ae
Nikhil Kansal <nkansal96@gmail.com>
2019-11-29 22:21:55 -0800

    remove unnecessary dict

--- tests/mahimahi/server/test_filestore.py
+++ tests/mahimahi/server/test_filestore.py
@@ -8,16 +8,16 @@ class TestCheckCacheability:
 
     def test_not_cacheable(self):
         tests = [
-            dict({CACHE_CONTROL_HEADER: "junk=123, max-age=0, junk4=1"}),
-            dict({CACHE_CONTROL_HEADER: "junk=123, junk4=1, max-age=0"}),
-            dict({CACHE_CONTROL_HEADER: "no-store, junk=123"}),
-            dict({CACHE_CONTROL_HEADER: "junk=123, no-cache"}),
-            dict({CACHE_CONTROL_HEADER: "junk=123, no-cache, max-age=123"}),
-            dict({PRAGMA_HEADER: "no-cache"}),
-            dict({EXPIRES_HEADER: "0"}),
-            dict({EXPIRES_HEADER: "0", CACHE_CONTROL_HEADER: "max-age=0"}),
-            dict({CACHE_CONTROL_HEADER: "max-age=asdf"}),
-            dict({CACHE_CONTROL_HEADER: "max-age=asdf", EXPIRES_HEADER: "0"}),
+            {CACHE_CONTROL_HEADER: "junk=123, max-age=0, junk4=1"},
+            {CACHE_CONTROL_HEADER: "junk=123, junk4=1, max-age=0"},
+            {CACHE_CONTROL_HEADER: "no-store, junk=123"},
+            {CACHE_CONTROL_HEADER: "junk=123, no-cache"},
+            {CACHE_CONTROL_HEADER: "junk=123, no-cache, max-age=123"},
+            {PRAGMA_HEADER: "no-cache"},
+            {EXPIRES_HEADER: "0"},
+            {EXPIRES_HEADER: "0", CACHE_CONTROL_HEADER: "max-age=0"},
+            {CACHE_CONTROL_HEADER: "max-age=asdf"},
+            {CACHE_CONTROL_HEADER: "max-age=asdf", EXPIRES_HEADER: "0"},
         ]
 
         for test in tests:
@@ -25,11 +25,11 @@ class TestCheckCacheability:
 
     def test_cacheable(self):
         tests = [
-            dict({CACHE_CONTROL_HEADER: "max-age=123"}),
-            dict({CACHE_CONTROL_HEADER: "max-age=asdf", EXPIRES_HEADER: "1600"}),
-            dict({EXPIRES_HEADER: "1600"}),
-            dict({LAST_MODIFIED_HEADER: "today"}),
-            dict({LAST_MODIFIED_HEADER: "today", CACHE_CONTROL_HEADER: "max-age=asdf"}),
+            {CACHE_CONTROL_HEADER: "max-age=123"},
+            {CACHE_CONTROL_HEADER: "max-age=asdf", EXPIRES_HEADER: "1600"},
+            {EXPIRES_HEADER: "1600"},
+            {LAST_MODIFIED_HEADER: "today"},
+            {LAST_MODIFIED_HEADER: "today", CACHE_CONTROL_HEADER: "max-age=asdf"},
         ]
 
         for test in tests:
