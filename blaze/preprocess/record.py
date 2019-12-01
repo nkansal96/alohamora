e2acbdcfb4cfc7d939e4d9e3f7367fb76a1e6e21
Murali Ramanujam <srmurali100@gmail.com>
2019-12-01 11:07:23 -0800

    use named arguments (#57)
    * use named arguments
    
    instead of positional
    
    * format

--- blaze/preprocess/record.py
+++ blaze/preprocess/record.py
@@ -153,7 +153,12 @@ def get_page_load_time_in_replay_server(
     for i in range(EXECUTION_CAPTURE_RUNS):
         log.debug("recording page execution in Mahimahi", run=(i + 1), total_runs=EXECUTION_CAPTURE_RUNS)
         har = capture_har_in_replay_server(
-            request_url, config, client_env, user_data_dir, policy, extract_critical_requests
+            url=request_url,
+            config=config,
+            client_env=client_env,
+            user_data_dir=user_data_dir,
+            policy=policy,
+            extract_critical_requests=extract_critical_requests,
         )
         hars.append(har)
         log.debug("captured page execution", page_load_time=har.page_load_time_ms)
@@ -183,7 +188,12 @@ def get_speed_index_in_replay_server(
     for i in range(EXECUTION_CAPTURE_RUNS):
         log.debug("recording page execution in Mahimahi", run=(i + 1), total_runs=EXECUTION_CAPTURE_RUNS)
         speed_index = capture_si_in_replay_server(
-            request_url, config, client_env, user_data_dir, policy, extract_critical_requests
+            url=request_url,
+            config=config,
+            client_env=client_env,
+            user_data_dir=user_data_dir,
+            policy=policy,
+            extract_critical_requests=extract_critical_requests,
         )
         speed_indices.append(speed_index)
         log.debug("captured page execution", speed_index=speed_index)
